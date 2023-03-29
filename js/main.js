fb12c7bcc48da31ad6045c332b8582f2940ff45b
montoya <ing.montoyacom@gmail.com>
2023-03-29 16:31:49 -0600

    botones

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
 // El código va aquí -> 
+let txtNombre = document.getElementById("Name");
+let txtNumber = document.getElementById("Number");
+
+let btnAgregar = document.getElementById("btnAgregar");
+let btnClear = document.getElementById("btnClear");
+
+btnClear.addEventListener("click", function (event) {
+    event.preventDefault();
+    txtNombre = "";
+    txtNumber = "";
+});
+
+btnAgregar.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (txtNombre.value.length == 0) {
+        txtNombre.style.border = "solid thin red";
+    } else {
+        txtNombre.style.border = "";
+    }
+
+});
+
+btnAgregar.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (txtNumber.value.length == 0) {
+        txtNumber.style.border = "solid thin red";
+    } else {
+        txtNumber.style.border = "";
+    }
+});
+
+txtNombre.addEventListener("blur", function (event) {
+    event.preventDefault();
+    txtNombre.value = txtNombre.value.trim();
+});
+
+txtNumber.addEventListener("blur", function (event) {
+    event.preventDefault();
+    txtNumber.value = txtNumber.value.trim();
+});
